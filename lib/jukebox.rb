da39afbf33b8e6cfd0630b7a44ee915dfd64c814
Dave Frame <dframe22@gmail.com>
2020-05-08 18:00:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,65 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts 'I accept the following commands:'
+  puts '- help : displays this help message'
+  puts '- list : displays a list of songs you can play'
+  puts '- play : lets you choose a song to play'
+  puts '- exit : exits this program'
+end
+
+def list(songs)
+  count = 0 
+  while count < songs.length
+    puts "#{count + 1}. #{songs[count]}"
+    count += 1
+  end
+end
+
+def play(songs)
+  puts 'Please enter a song name or number:'
+  reply = gets.strip
+  if reply.to_i > 0 && reply.to_i < songs.length
+    puts "Playing #{songs[reply.to_i - 1]}"
+  elsif songs.include?(reply)
+    puts "Playing #{reply}"
+  else 
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts 'Goodbye'
+end
+
+def run 
+  puts "Please enter a command:"
+  reply = gets.strip
+  while reply != 'exit'
+    case reply
+      when "help"
+        help
+        run
+      when "list"
+        list
+        run
+      when "play"
+        play
+        run
+    else
+      puts "Invalid. Type 'help' for list of commands."
+      run
+    end
+  end
+  exit_jukebox
+end
